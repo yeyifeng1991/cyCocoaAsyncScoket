46ce5bd30368b6def7cf04709a4f67f0bcad15cc
叶子 <yeyifeng@192.168.1.8>
2018-02-28 23:41:18 +0800

    first commit

---
+++ cyCocoaSyncSocket/cySocketManager.h
@@ -0,0 +1,13 @@
+//
+//  cySocketManager.h
+//  cyCocoaSyncSocket
+//
+//  Created by 叶子 on 2018/2/28.
+//  Copyright © 2018年 叶子. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cySocketManager : NSObject
+
+@end
---
+++ cyCocoaSyncSocket/cySocketManager.m
@@ -0,0 +1,13 @@
+//
+//  cySocketManager.m
+//  cyCocoaSyncSocket
+//
+//  Created by 叶子 on 2018/2/28.
+//  Copyright © 2018年 叶子. All rights reserved.
+//
+
+#import "cySocketManager.h"
+
+@implementation cySocketManager
+
+@end
